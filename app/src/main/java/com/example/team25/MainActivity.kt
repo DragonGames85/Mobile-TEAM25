856b76683f2552d4cbedd5b2f46cc0c2bf63ded5
DragonGames85 <dragongames85@mail.ru>
2022-04-22 16:12:08 +0700

    Visual part 1

--- app/src/main/java/com/example/team25/MainActivity.kt
+++ app/src/main/java/com/example/team25/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.example.team25
-
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.example.team25.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindi: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindi = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindi.root)
+        bindi.bNav?.selectedItemId = R.id.item3
+        bindi.bNav?.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.item1 -> {
+                    Toast.makeText(this, "launched", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item2 -> {
+                    Toast.makeText(this, "launched", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item3 -> {
+                    Toast.makeText(this, "launched", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            android.R.id.home -> finish()
+            R.id.launch -> {
+                Toast.makeText(this, "launched", Toast.LENGTH_SHORT).show()
+
+            }
+            R.id.clear -> {
+                Toast.makeText(this, "cleared", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return true
+    }
+
+//    fun Launch(view : View) {
+//        var tes=findViewById<TextView>(R.id.Textr)
+//        tes.text =  "JO"
+//    }
 }
